66fbb550cd02a8fafd8fbc58b60db91b7829a344
cchois <94801991+cchois@users.noreply.github.com>
2025-02-15 12:39:55 -0500

    Delete formulario directory

--- formulario/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('miFormulario').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulario enviado con éxito');
-    // Aquí puedes agregar lógica para enviar los datos a un servidor
-});
