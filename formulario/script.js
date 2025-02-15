9c11c6c546af1362760a20c4fd8fd221e8839bd0
cchois <94801991+cchois@users.noreply.github.com>
2025-02-15 12:33:29 -0500

    Create script.js

---
+++ formulario/script.js
@@ -0,0 +1,5 @@
+document.getElementById('miFormulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado con éxito');
+    // Aquí puedes agregar lógica para enviar los datos a un servidor
+});
